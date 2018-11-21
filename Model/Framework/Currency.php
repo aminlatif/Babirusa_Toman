ed0c97bd7c34634872fd3b0ac8d37f0b95805e90
aminlatif <amin.latifkar@gmail.com>
2018-11-21 16:07:24 +0330

    Solved Thousand Separator Problem

--- Model/Framework/Currency.php
+++ Model/Framework/Currency.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Babirusa\Toman\Model\Framework;
 
 use Magento\Framework\App\CacheInterface;
@@ -9,26 +10,33 @@ class Currency extends \Magento\Framework\Currency
         CacheInterface $appCache,
         $options = null,
         $locale = null
-    ) {
-        if($options=="IRT") {
+    )
+    {
+        if ($options == "IRT") {
             $options = array();
             $options["precision"] = 0;
             $options["name"] = "Iranian Toman";
             $options["currency"] = "IRT";
             $options["symbol"] = "Toman";
-            $options["format"] = "#،##0.00 ¤";
-            //$options["format"] = "#,##0.00 ¤";
-            if($locale=="fa"||$locale=="fa_IR"){
+            //$options["format"] = "#،##0.00 ¤";
+            $options["format"] = "#,##0.00 ¤";
+            if ($locale == "fa" || $locale == "fa_IR") {
                 $options["symbol"] = "تومان";
             }
         }
 
         parent::__construct($appCache, $options, $locale);
     }
+
     public function toCurrency($value = null, array $options = array())
     {
-        if($this->_options['currency']=='IRT') {
-            $options["precision"] = 0;
+        $locale = $this->getLocale();
+        if ($this->_options['currency'] == 'IRT') {
+            if ($locale == "fa" || $locale == "fa_IR") {
+                $options["precision"] = 0;
+            }else{
+                $options["precision"] = 2;
+            }
         }
         $currencyStr = trim(parent::toCurrency($value, $options));
         //$currencyStr = str_replace(",", "،", $currencyStr);
